1d8f5229b3197f9848f74f6876ffba1435b35582
haslan82 <haslan82@hotmail.com>
2024-07-06 20:58:31 +0300

    first commit

---
+++ src/firebase/index.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD57vc1y5XnE7d1pVVS0KKzwKl5hQnk1pU",
+  authDomain: "x-clone-ed916.firebaseapp.com",
+  projectId: "x-clone-ed916",
+  storageBucket: "x-clone-ed916.appspot.com",
+  messagingSenderId: "756148965794",
+  appId: "1:756148965794:web:c9bdedfb752ecb421a62fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// kimlik doğrulama yapısının referansını al
+
+export const auth = getAuth(app);
+
+// google provider kurulumu
+export const provider = new GoogleAuthProvider();
