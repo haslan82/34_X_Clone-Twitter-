f71f534d6e2736f1f22083fee446a51166d220b9
haslan82 <haslan82@hotmail.com>
2024-07-11 22:43:12 +0300

    ekledim

--- src/components/Post/Buttons.jsx
+++ src/components/Post/Buttons.jsx
@@ -2,8 +2,40 @@ import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
 import { CiShare2 } from "react-icons/ci";
 import { FaHeart, FaRegHeart, FaRetweet } from "react-icons/fa";
 import { LuMessageCircle } from "react-icons/lu";
+import { db, auth } from "../../firebase";
 
-const Buttons = () => {
+
+//! console.log(auth)
+
+const Buttons = ({tweet}) => {
+
+  // oturumu açık olan kullanıcı bu tweet i like ladı mı?
+const isLiked = tweet.likes.includes(auth.currentUser.uid);
+  //!  console.log(auth.currentUser.uid)
+
+
+
+  //!  console.log(tweet.likes)
+
+  // 1) 31. satırda onclick verdikten sonra buraya gelip // like durumunu terine çevirecek fonksiyon yaz
+  const toggleLike = async ()=>{
+
+
+// 2) güncellenecek dökümanın referansını al
+const tweetRef  = doc(db, "tweets", tweet.id);
+
+  // 3) referansı alınan tweet dökümanını güncelle
+await updateDoc(tweetRef, {
+ // isEdited:true,
+ // textContent: "Yeni yazı içeriği",
+
+    likes: 
+    isLiked 
+    ? arrayRemove(auth.currentUser.uid) 
+    : arrayUnion(auth.currentUser.uid),
+
+})
+};
   return (
     <div className="flex justify-between items-center">
    <div className="p-3 rounded-full cursor-pointer
@@ -14,9 +46,12 @@ const Buttons = () => {
     transition hover:bg-[#000ff44]">
         <FaRetweet />
     </div>
-   <div className="p-3 rounded-full cursor-pointer
+   <div onClick={toggleLike} className="p-3 flex gap-2 items-center rounded-full cursor-pointer
     transition hover:bg-[#ff00d444]">
-        <FaRegHeart />
+
+      {isLiked ? <FaHeart className="text-red-500" /> : 
+        <FaRegHeart />}
+        <p>{tweet.likes.length} </p>
     </div>
    <div className="p-3 rounded-full cursor-pointer
     transition hover:bg-[#3b3b3bac]">
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,9 +1,20 @@
+import { auth } from "../../firebase";
 import Buttons from "./Buttons";
 import Content from "./Content";
 import Dropdown from "./Dropdown";
 import UserInfo from "./UserInfo";
 
 const Post = ({ tweet }) => {
+  
+// tweet'i mevcut (oturumu açık olan) kullanıcı mı attı?
+  const isOwn = tweet.user.id === auth.currentUser.uid;
+
+
+//! console.log(tweet.user.id);
+//! console.log(auth.currentUser.uid);
+
+
+
   return (
     <div className="flex gap-3 border-b py-6 px-3 border-zinc-600">
       <img
@@ -15,7 +26,8 @@ const Post = ({ tweet }) => {
       <div className="w-full">
         <div className="flex justify-between">
           <UserInfo tweet={tweet} />
-          <Dropdown tweet={tweet} />
+          {isOwn &&  <Dropdown tweet={tweet} />}
+         
         </div>
 
         <Content tweet={tweet} />
